63745cde27cc43115b23a0426e9ec14bfed1dd5a
julianR92 <julianrincon9230@gmail.com>
2022-09-05 14:21:31 -0500

    primera version

---
+++ appema.sql
@@ -0,0 +1,161 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 05-09-2022 a las 19:15:34
+-- Versión del servidor: 5.7.33
+-- Versión de PHP: 8.0.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `appema`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `gender` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `number` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `city` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `estado` bigint(1) DEFAULT NULL COMMENT 'Estado del usuario\r\n',
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `gender`, `email`, `password`, `address`, `number`, `city`, `estado`, `email_verified_at`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', 'User', NULL, 'ojrincon@bucaramanga.gov.co', '$2y$10$LueqldPmcKdhoK7I9uMZzOpTUUQ71fSUd/w2TNXlrw7TAEC3x6ExC', NULL, NULL, NULL, 0, NULL, 'VZRMC0PaCZTVY0F7wJxPqbwhCvf2hJ8FAjJO0bhQr6v6aJ2Jea0845dzg87n', NULL, '2022-09-02 03:51:11'),
+(2, 'Julian', 'Rincon', NULL, 'julianrincon9230@gmail.com', '$2y$10$g5BAX2rOZCTw1.ay.HxEm.XrZsgYyT8i7/21o1PM3iSqehHIMmSbW', NULL, NULL, NULL, 1, NULL, '0sta60K7F9twTmsEO3FUooP4acgJyR52v3q5tzdh9CV9vbGHReEhmWKNtKBf', '2022-08-31 07:51:21', '2022-09-05 13:40:47');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indices de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_id_unique` (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD UNIQUE KEY `users_remember_token_unique` (`remember_token`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ resources/views/layouts/footer2.blade.php
@@ -0,0 +1,112 @@
+{{-- <div class="theme-settings card bg-gray-800 pt-2 collapse" id="theme-settings">
+    <div class="card-body bg-gray-800 text-white pt-4">
+        <button type="button" class="btn-close theme-settings-close" aria-label="Close" data-bs-toggle="collapse"
+            href="#theme-settings" role="button" aria-expanded="false" aria-controls="theme-settings"></button>
+        <div class="d-flex justify-content-between align-items-center mb-3">
+            <p class="m-0 mb-1 me-4 fs-7">Open source <span role="img" aria-label="gratitude">💛</span></p>
+            <a class="github-button" href="https://github.com/themesberg/volt-laravel-admin-dashboard"
+                data-color-scheme="no-preference: dark; light: light; dark: light;" data-icon="octicon-star"
+                data-size="large" data-show-count="true"
+                aria-label="Star themesberg/volt-bootstrap-5-dashboard on GitHub">Star</a>
+        </div>
+        <a href="https://themesberg.com/product/laravel/volt-admin-dashboard-template" target="_blank"
+            class="btn btn-secondary d-inline-flex align-items-center justify-content-center mb-3 w-100">
+            Download
+            <svg class="icon icon-xs ms-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                    d="M2 9.5A3.5 3.5 0 005.5 13H9v2.586l-1.293-1.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 15.586V13h2.5a4.5 4.5 0 10-.616-8.958 4.002 4.002 0 10-7.753 1.977A3.5 3.5 0 002 9.5zm9 3.5H9V8a1 1 0 012 0v5z"
+                    clip-rule="evenodd"></path>
+            </svg>
+        </a>
+        <p class="fs-7 text-gray-300 text-center">Available in the following technologies:</p>
+        <div class="d-flex justify-content-center">
+            <a class="me-3" href="https://themesberg.com/product/admin-dashboard/volt-bootstrap-5-dashboard"
+                target="_blank">
+                <img src="/assets/img/technologies/bootstrap-5-logo.svg" class="image image-xs">
+            </a>
+            <a href="https://demo.themesberg.com/volt-react-dashboard/#/" target="_blank">
+                <img src="/assets/img/technologies/react-logo.svg" class="image image-xs">
+            </a>
+        </div>
+    </div>
+</div> --}}
+{{-- <div class="card theme-settings bg-gray-800 theme-settings-expand" id="theme-settings-expand">
+    <div class="card-body bg-gray-800 text-white rounded-top p-3 py-2">
+        <span class="fw-bold d-inline-flex align-items-center h6">
+            <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                    d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+                    clip-rule="evenodd"></path>
+            </svg>
+            Settings
+        </span>
+    </div>
+</div> --}}
+<footer>
+<div class="container-fluid px-0"> 
+    <div class="footer page__footer footer-white footer-blue">
+       <div class="container">
+          <div class="footer-container" id="footer-container">
+
+             <div class="nav-footer icon-white  nav-pills nav-pills-icons icon-white d-flex justify-content-center bd-highlight">
+                <div class="item-footer border-right col-md-2">
+                   <div class="d-flex justify-content-center">
+                      <a class="navbar-brand" href="https://www.gov.co/" style="padding:0!important;">
+                         <img src="https://cdn.www.gov.co/assets/images/logo.png" height="40" alt="Logo Gov.co" />
+                      </a>
+                   </div>
+                   <div class="d-flex justify-content-center mt-3">
+                      <a class="" href="https://www.gov.co/" style="padding:0!important;">
+                         <img src="{{ asset('assets/img/imct/logo_colombia.png') }}" height="60px" alt="Logo Gov.co" />
+                      </a>
+                   </div>
+                </div>
+                <div class="item-footer col-md-5 border-right">
+                   <div class="px-3">
+                      <p class="font-weight-bold">Instituto Municipal de Cultura y Turismo de Bucaramanga</p>
+                      <p><b>Nit:</b> 890204594-9</p>
+                      <p><b>Dirección:</b> Calle 30 # 26 – 117, Parque de los niños, Bucaramanga, Santander-Colombia.</p>                  
+                      <p><b>Horario de Atención:</b>  Lunes a viernes entre 8:00am – 12:00am y 1:00pm – 4:45pm</p>
+                      <br>
+                      <div class="row">
+                         <div class="col-md-12"><p class="font-weight-bold">Redes Sociales</p></div>
+                         <div class="col-md-1">                            
+                            <a href="https://www.facebook.com/imct.bucaramanga/"><i class="fab fa-facebook fa-2x text-white"></i></a>
+                           
+                         </div>                          
+                        
+                         <div class="col-md-1">                              
+                            <a href="https://twitter.com/imctbga"><i class="fab fa-twitter-square fa-2x text-white"></i></a>
+                         </div>
+                         
+                         <div class="col-md-1">
+                            <a href="https://www.instagram.com/imctbga/"><i class="fab fa-instagram fa-2x text-white"></i></a>
+                         </div>
+                         <div class="col-md-1">
+                            {{-- <a href="https://www.youtube.com/user/PrensaBucaramanga"><i class="fab fa-youtube fa-2x text-white"></i></a> --}}
+                         </div>
+                      </div>
+                   </div>
+                </div>
+                <div class="item-footer col-md-3">
+                   <div class="px-3">
+                      <p class="font-weight-bold">Contacto</p>
+                      <p><b>Conmutador:</b> <a href="tel:6076059460" style="color: white;"> (607) 6059460</a></p>                    
+                      <p><b>Correo Institucional:</b> <a href="mailto:contactenos@imct.gov.co" style="color: white;">contactenos@imct.gov.co</a></p>
+                      <b>Correo de notificaciones judiciales:</b> <a href="mailto:notificacionesjudiciales@imct.gov.co" style="color: white;" >notificacionesjudiciales@imct.gov.co</a></p>
+                      <p><a href="https://imct.gov.co/wp-content/uploads/2017/08/Politica-y-resolucion-RNBD.pdf" target="_blank" rel="noopener" style="color: white; text-decoration:underline ">Politicas de tratamiento de datos</a></p>
+                      <p><a href="http://www.imct.gov.co/mapaSitio.html" target="_blank" rel="noopener" style="color: white; text-decoration:underline ">Mapa del sitio</a></p>
+                   </div>
+                </div>
+                
+             </div>
+             {{-- <div class="row pt-5 mb-0 pb-0">
+                <div class="col-md-1"></div>
+                <div class="col-md-10"><p class="font-weight-bold" style="text-align:center;"><a class="text-white" href="https://www.bucaramanga.gov.co/autorizacion-de-tratamiento-de-datos-personales/" target="_blank" >Autorización de tratamiento de datos personales |</a>&nbsp;<a class="text-white" href="https://www.bucaramanga.gov.co/wp-content/uploads/2018/12/Resolucion-340-Dic-26-2018-y-Politica.pdf" target="_blank" >Política de tratamiento de datos personales |</a>&nbsp; <a class="text-white" href="https://www.bucaramanga.gov.co/condiciones-de-uso/" target="_blank" >Política web y condiciones de uso |</a>&nbsp; <a class="text-white" href="https://www.bucaramanga.gov.co/wp-content/uploads/2021/08/Politica_Editorial_Actualizada_2014.pdf" target="_blank" >Política Editorial |</a>&nbsp; <a class="text-white" href="https://www.bucaramanga.gov.co/wp-content/uploads/2021/08/Plan_de_Uso_Redes_Sociales.pdf" target="_blank">Plan de uso de redes sociales |</a>&nbsp;  <a class="text-white" href="https://www.bucaramanga.gov.co/wp-content/uploads/2021/08/PLAN-DE-COMUNICACIONES-2020-2023.pdf" target="_blank">Plan de comunicaciones |</a>&nbsp;<a class="text-white" href="https://www.bucaramanga.gov.co/wp-content/uploads/2021/08/PLAN-DE-COMUNICACIONES-2020-2023.pdf" target="_blank">Política de seguridad de la información |</a>&nbsp; <a class="text-white" href="https://www.bucaramanga.gov.co/politicas-de-privacidad/" target="_blank">Uso y monitoreo de la página web |</a>&nbsp; <a class="text-white" href="https://www.gov.co/home/mapa-del-sitio" target="_blank">Mapa del sitio</a> </p></div>
+                <div class="col-md-1"></div>
+             </div> --}}
+          </div>
+       </div>
+    </div>
+ </div>
+ </footer>
